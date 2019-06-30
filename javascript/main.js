898a4ff15b45dac000f53e60836eabda6b98285d
Connor Perez <connorperez@gmail.com>
2019-06-30 15:12:53 -0500

    adding to do list part

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+// Add button in header to add activity to list
+document.getElementById('add').addEventListener('click', function(){
+    var value = document.getElementById('item').value;
+    if(value){
+    }
+})
