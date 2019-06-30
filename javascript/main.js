2887bc8ceec41b6d0dcfdf69f3eae0a40fb7af3b
Connor Perez <connorperez@gmail.com>
2019-06-30 15:53:50 -0500

    new items appending

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,34 @@
+
+
+
 // Add button in header to add activity to list
 document.getElementById('add').addEventListener('click', function(){
     var value = document.getElementById('item').value;
-    if(value){
-    }
-})
+    if(value) addItemTodo(value)
+
+    
+});
+
+// Adds new todo item to list
+function addItemTodo(text){
+
+    var list = document.getElementById('todo');
+
+    var item = document.createElement('li');
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    
+    var complete = document.createElement('button');
+    remove.classList.add('compelte');
+
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    item.appendChild(buttons);
+
+    list.appendChild(item);
+}
